4a828bf3ed5a621b50c55f3ea459dc024e216050
Luna Sarmiento <selene942008@hotmail.com>
2021-08-16 14:40:56 +0200

    validation and error handling done

--- src/server.js
+++ src/server.js
@@ -1,19 +1,29 @@
-import express from "express"
-import cors from "cors"
-import Products from "./products/index.js"
-import Reviews from "./reviews/index.js"
+import express from "express";
+import cors from "cors";
+import Products from "./products/index.js";
+import Reviews from "./reviews/index.js";
+import {
+  notFoundErrorHandler,
+  forbiddenErrorHandler,
+  badRequestErrorHandler,
+  genericServerErrorHandler,
+} from "./errorHandlers.js";
 
-const server = express()
+const server = express();
 
-const port = 3005
+const port = 3005;
 
-server.use(cors())
-server.use(express.json()) 
+server.use(cors());
+server.use(express.json());
 
+server.use("/products", Products);
+server.use("/reviews", Reviews);
 
-server.use("/products", Products)
-server.use("/reviews", Reviews)
+server.use(notFoundErrorHandler);
+server.use(badRequestErrorHandler);
+server.use(forbiddenErrorHandler);
+server.use(genericServerErrorHandler);
 
 server.listen(port, () => {
-    console.log("Server Up")
-})
+  console.log("Server Up");
+});
