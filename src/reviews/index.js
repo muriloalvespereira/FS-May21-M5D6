24c2cd81d307651abf61c29c632efa582da9c38a
Luna Sarmiento <selene942008@hotmail.com>
2021-08-16 12:31:42 +0200

    crud reviews done

--- src/reviews/index.js
+++ src/reviews/index.js
@@ -1,5 +1,97 @@
-import express from "express"
+import express from "express";
+import fs from "fs-extra";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import uniqid from "uniqid";
 
-const Reviews = express.Router()
+const Reviews = express.Router();
 
-export default Reviews
+const { readJSON, writeJSON } = fs;
+
+Reviews.get("/", async (req, resp, next) => {
+  try {
+    const reviewsPath = join(
+      dirname(fileURLToPath(import.meta.url)),
+      "reviews.json"
+    );
+    const reviews = await readJSON(reviewsPath);
+    console.log(reviews);
+    resp.send(reviews);
+  } catch (err) {
+    next(err);
+  }
+});
+
+Reviews.get("/:_id", async (req, resp, next) => {
+  try {
+    const reviewsPath = join(
+      dirname(fileURLToPath(import.meta.url)),
+      "reviews.json"
+    );
+    const reviews = await readJSON(reviewsPath);
+    console.log(reviews);
+    const singleReview = await reviews.find((r) => r._id === req.params._id);
+    if (singleReview) {
+      resp.send(singleReview);
+    } else {
+      console.log("Something went wrong");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+Reviews.post("/", async (req, resp, next) => {
+  try {
+    const reviewsPath = join(
+      dirname(fileURLToPath(import.meta.url)),
+      "reviews.json"
+    );
+    const reviews = await readJSON(reviewsPath);
+    const newReview = { ...req.body, _id: uniqid(), createdAt: new Date() };
+    const createReview = (content) => writeJSON(reviewsPath, content);
+    reviews.push(newReview);
+    await createReview(reviews);
+    resp.send(reviews);
+    console.log(reviews);
+  } catch (err) {
+    next(err);
+  }
+});
+
+Reviews.put("/:_id", async (req, resp, next) => {
+  try {
+    const reviewsPath = join(
+      dirname(fileURLToPath(import.meta.url)),
+      "reviews.json"
+    );
+    const reviews = await readJSON(reviewsPath);
+    const remainingReviews = reviews.filter((r) => r._id !== req.params._id);
+    const modifiedReview = { ...req.body, _id: req.params._id };
+    console.log(modifiedReview);
+    remainingReviews.push(modifiedReview);
+    const changeReview = (content) => writeJSON(reviewsPath, content);
+    await changeReview(remainingReviews);
+    resp.send(modifiedReview);
+  } catch (err) {
+    next(err);
+  }
+});
+
+Reviews.delete("/:_id", async (req, resp, next) => {
+  try {
+    const reviewsPath = join(
+      dirname(fileURLToPath(import.meta.url)),
+      "reviews.json"
+    );
+    const reviews = await readJSON(reviewsPath);
+    const remainingReviews = reviews.filter((r) => r._id !== req.params._id);
+    const changeReview = (content) => writeJSON(reviewsPath, content);
+    await changeReview(remainingReviews);
+    resp.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default Reviews;
