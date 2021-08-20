727920b34581e23b422bf7dc0cce4cfaca14c053
Murilo Alves <murilo.udi@gmail.com>
2021-08-20 18:36:58 +0200

    Added a try catch

--- src/lib/pdf.js
+++ src/lib/pdf.js
@@ -1,20 +1,20 @@
-import PdfPrinter from "pdfmake"
-import { pipeline } from "stream"
-import fs from "fs-extra"
-import { join, dirname } from "path"
-import { fileURLToPath } from "url"
-import { promisify } from "util"
-import ImageDataURI  from 'image-data-uri'
-import htmlToPdfmake from "html-to-pdfmake"
-import jsdom from 'jsdom'
+import PdfPrinter from "pdfmake";
+import { pipeline } from "stream";
+import fs from "fs-extra";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { promisify } from "util";
+import ImageDataURI from "image-data-uri";
+import htmlToPdfmake from "html-to-pdfmake";
+import jsdom from "jsdom";
 
 const { JSDOM } = jsdom;
 const { window } = new JSDOM("");
 
 const convertImageURL = async (imageUrl) => {
-    const encodedUrl = await ImageDataURI.encodeFromURL(imageUrl) 
-    return encodedUrl
-}
+  const encodedUrl = await ImageDataURI.encodeFromURL(imageUrl);
+  return encodedUrl;
+};
 
 const fonts = {
   Roboto: {
@@ -23,48 +23,59 @@ const fonts = {
     italics: "Helvetica-Oblique",
     bolditalics: "Helvetica-BoldOblique",
   },
-}
+};
 
 export const getPDFReadableStream = async (data, req) => {
-  const asyncPipeline = promisify(pipeline)
-  const printer = new PdfPrinter(fonts)
-  let encondedImage = await convertImageURL(data[0].imageUrl)
+  try {
+    const asyncPipeline = promisify(pipeline);
+    const printer = new PdfPrinter(fonts);
+    let encondedImage = await convertImageURL(data[0].imageUrl);
 
-  const docDefinition = {
-    pageSize: 'A4',
-    pageMargins: [40, 60, 40, 60],
-    // header : `${pdfData.title}`, just playing around
-    content: [
-        { //styling
-            text: `${data[0].name}`,
-            alignment: 'center',
-            // margin: [left, top, right, bottom]
-            margin: [ 0, 0, 0, 24 ],
-            fontSize: 24, 
-            bold: true
+    const docDefinition = {
+      pageSize: "A4",
+      pageMargins: [40, 60, 40, 60],
+      // header : `${pdfData.title}`, just playing around
+      content: [
+        {
+          //styling
+          text: `${data[0].name}`,
+          alignment: "center",
+          // margin: [left, top, right, bottom]
+          margin: [0, 0, 0, 24],
+          fontSize: 24,
+          bold: true,
         },
         {
-            image: encondedImage,
-            fit: [515, 900]
+          image: encondedImage,
+          fit: [515, 900],
         },
         {
-            //convert html into text for pdf maker needs to pass the window to the htmlToPdfmaker when using node
-            text: htmlToPdfmake(data[0].description, {window:window}),
-            margin: [ 0, 24, 0, 0 ],
-            fontSize: 16,
-            lineHeight: 1.4 
-        }
-    ]
-}
-  const options = {}
-  const pdfReadableStream = printer.createPdfKitDocument(docDefinition, options)
+          //convert html into text for pdf maker needs to pass the window to the htmlToPdfmaker when using node
+          text: htmlToPdfmake(data[0].description, { window: window }),
+          margin: [0, 24, 0, 0],
+          fontSize: 16,
+          lineHeight: 1.4,
+        },
+      ],
+    };
+    const options = {};
+    const pdfReadableStream = printer.createPdfKitDocument(
+      docDefinition,
+      options
+    );
 
-  pdfReadableStream.end()
-  const path = join(dirname(fileURLToPath(import.meta.url)), `../../public/pdf/${req.params.productId}.pdf`)
-  // const path = join(dirname(fileURLToPath(import.meta.url)), "test.pdf")
-  await asyncPipeline(pdfReadableStream, fs.createWriteStream(path)) // generatePDFAsync will await for the stream to end before returning
-  return path
-}
+    pdfReadableStream.end();
+    const path = join(
+      dirname(fileURLToPath(import.meta.url)),
+      `../../public/pdf/${req.params.productId}.pdf`
+    );
+    // const path = join(dirname(fileURLToPath(import.meta.url)), "test.pdf")
+    await asyncPipeline(pdfReadableStream, fs.createWriteStream(path)); // generatePDFAsync will await for the stream to end before returning
+    return path;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // export const generatePDFAsync = async data => {
 //   const asyncPipeline = promisify(pipeline) // promisify is an utility which is going to transform a function that uses callbacks into a function that uses Promises (and so Async/Await). Pipeline is a function that works with callbacks to connect two or more streams together --> I can promisify pipeline getting back an "asynchronous pipeline"
